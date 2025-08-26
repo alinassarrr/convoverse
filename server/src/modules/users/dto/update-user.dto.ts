950b97e9e4294fedd04a47233a9d69e438babd0e
alinassarrr <aliinassar8@gmail.com>
2025-08-27 00:12:40 +0300

    feat(User): validate creation of user

--- server/src/modules/users/dto/update-user.dto.ts
+++ server/src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty()
+  @IsString()
+  fullname?: string | undefined;
+}
