2440d565da16d51349ea225c3eeb77a0a18cb2da
alinassarrr <aliinassar8@gmail.com>
2025-08-27 00:12:08 +0300

    feat(User): validate user credentials

--- server/src/modules/users/dto/create-user.dto.ts
+++ server/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  fullname: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(20, { message: 'Password must not be longer than 20 characters' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password too weak. It must include an uppercase letter, a lowercase letter, and a number or special character.',
+  })
+  password: string;
+}
