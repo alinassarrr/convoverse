221a9feab8f9224cffdc329f8a9a277f5416c8e0
alinassarrr <aliinassar8@gmail.com>
2025-08-31 12:53:51 +0300

    feat(Auth): import mongoose modules for user and refreshtoken

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schemas/users.schema';
+import {
+  RefreshToken,
+  RefreshTokenSchema,
+} from 'src/schemas/refresh-token.schema';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+      {
+        name: RefreshToken.name,
+        schema: RefreshTokenSchema,
+      },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
