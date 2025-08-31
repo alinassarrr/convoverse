fd9870ab60d9f6442c2d40a4156fc1f507101a8c
alinassarrr <aliinassar8@gmail.com>
2025-08-31 12:48:45 +0300

    feat(App): configure global settings with ConfigModule and update imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 import { UsersModule } from './modules/users/users.module';
 
-const uri = 'mongodb://127.0.0.1:27017/convoverse';
-
 @Module({
-  imports: [MongooseModule.forRoot(uri), UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+  ],
 })
 export class AppModule {}
