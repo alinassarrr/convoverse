6a77a0f50c2986935de592317c65394d47e9569b
alinassarrr <aliinassar8@gmail.com>
2025-08-31 12:57:19 +0300

    feat(Auth): implement the business logic behind signup

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -22,5 +22,20 @@ export class AuthService {
     private jwtService: JwtService,
   ) {} // we injected user service in auth module
 
-  async signup(credentials: SignUpDTO) {}
+  async signup(credentials: SignUpDTO) {
+    const { fullname, email, password } = credentials;
+    // Todo: check if email already used
+    const usedEmail = await this.UserModel.findOne({
+      email,
+    });
+    if (usedEmail) throw new BadRequestException('Email already in use');
+    // Todo: hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+    // Todo: create  user document and save in DB
+    await this.UserModel.create({
+      fullname,
+      email,
+      password: hashPassword,
+    });
+  }
 }
