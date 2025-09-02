04ef6cbc012cc747b2d44fd71516c1f8d3f084c0
alinassarrr <aliinassar8@gmail.com>
2025-09-02 17:04:41 +0300

    feat(Config): add slack configuration with clientID,clientSeceret, and redirectUri

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,4 +5,9 @@ export default () => ({
   database: {
     connectionString: process.env.MONGO_URL,
   },
+  slack: {
+    clientId: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    redirectUri: process.env.SLACK_REDIRECT_URI,
+  },
 });
