5afc6715d5ed20ada5a49b288fb93cc89d175552
alinassarrr <aliinassar8@gmail.com>
2025-09-19 04:35:52 +0300

    feat(GatewaysModule): add Action and Summary schemas to Mongoose module imports

--- server/src/gateways/gateways.module.ts
+++ server/src/gateways/gateways.module.ts
@@ -3,13 +3,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ChatGateway } from './chat.gateway';
 import { DatabaseWatcherService } from '../services/database-watcher.service';
 import { Message, MessageSchema } from '../schemas/messages.schema';
-import { Conversation, ConversationSchema } from '../schemas/conversations.schema';
+import {
+  Conversation,
+  ConversationSchema,
+} from '../schemas/conversations.schema';
+import { Action, ActionSchema } from '../schemas/action.schema';
+import { Summary, SummarySchema } from '../schemas/summarys.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Message.name, schema: MessageSchema },
       { name: Conversation.name, schema: ConversationSchema },
+      { name: Action.name, schema: ActionSchema },
+      { name: Summary.name, schema: SummarySchema },
     ]),
   ],
   providers: [ChatGateway, DatabaseWatcherService],
